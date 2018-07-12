7f49057f7a70d677b0c61af17bee6a5e778b77ae
scmc421 <scmc.421@gmail.com>
2018-07-12 15:35:23 -0500

    estableciendo la pagina principal

--- Backend/buhos0.1/config/routes.rb
+++ Backend/buhos0.1/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'wellcome/index'
+root :to => "wellcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
